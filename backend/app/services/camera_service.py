f276bec7b73d12768876644e621b1294913ed781
Nguyễn Văn Minh Giang <99959855+NguyenVanMinhGiang2005@users.noreply.github.com>
2025-12-25 23:07:20 +0700

    Update camera_service.py

--- backend/app/services/camera_service.py
+++ backend/app/services/camera_service.py
@@ -1,31 +1,63 @@
-from bson import ObjectId
-from app.database.mongo import get_db
+import json
+import os
+from typing import Any
+from app.core.config import settings
 
-def _oid_to_str(doc: dict) -> dict:
-    doc["id"] = str(doc["_id"])
-    doc.pop("_id", None)
-    return doc
+# khởi tạo bộ nhớ đệm 
+_cache: list[dir[str, Any]] | None = None # lần đầu lưu nội dung của json để tái sử dụng 
+_cache_mtime: float | None = None # lưu lại thời gian để biết khi nào phải thay đổi để cập nhập lại _cache
+
+
+def _load_json() -> list[dir[str, Any]]:
+    global _cache, _cache_mtime
+
+    path = settings.DATA
+    if not os.path.isabs(path):
+        path = os.path.join(os.getcwd(), path) # print(path) -> C:\Users\{Username}\Project\data\file.txt (vi du)
+    
+    st = os.stat(path) # get in4 file 
+    if _cache is not None and _cache_mtime == st.st_mtime:
+        return _cache
+    
+    # đọc file
+    with open(path, "r", encoding="utf-8") as f:
+        data = json.load(f)
+
+    if not isinstance(data, list):
+        raise ValueError("data.json must be a JSON array (list of cameras)")
+    
+    normalized = []
+    for item in data:
+        if not isinstance(item, dict): #check key-value
+            continue
+
+        id = item.get("_id", "")
+        if isinstance(id, dict) and "$oid" in id:
+            clean_id = id["$oid"]
+
+        normalized.append({
+            "id": clean_id,
+            "name": item.get("name", ""),
+            "location": item.get("location", None),
+            "url": item.get("url", ""),
+        })
+    
+    _cache = normalized
+    _cache_mtime = st.st_mtime
+    return normalized
 
 class CameraService:
     @staticmethod
-    async def list_cameras(limit: int = 50, skip: int = 0) -> tuple[list[dict], int]:
-        db = get_db()
-        col = db["link"]
-
-        cursor = col.find({}, {"name": 1, "url": 1, "location": 1}).skip(skip).limit(limit)
-        items = [ _oid_to_str(doc) async for doc in cursor ]
-        total = await col.count_documents({})
-        return items, total
-
+    async def list_camera(skip: int = 50, limit: int = 0) -> tuple[list[dict],int]:
+        items = _load_json()
+        total = len(items) # số lượng camera
+        return items[skip: skip + limit], total
+    
     @staticmethod
     async def get_camera(camera_id: str) -> dict | None:
-        db = get_db()
-        col = db["link"]
-
-        try:
-            oid = ObjectId(camera_id)
-        except Exception:
-            return None
-
-        doc = await col.find_one({"_id": oid}, {"name": 1, "url": 1, "location": 1})
-        return _oid_to_str(doc) if doc else None
+        item = _load_json()
+        for cam in item:
+            if cam == camera_id:
+                return cam
+        
+        return None
